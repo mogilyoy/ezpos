71ced5bf23b5037249343dd36cb999b5d25eb2a3
mogilyoy <mogilyoy@ya.ru>
2025-09-19 10:06:17 +0500

    product id: int

--- ezpos/models/sale.py
+++ ezpos/models/sale.py
@@ -18,7 +18,7 @@ class SaleStatusEnum(str):
 
 class Product(BaseModel):
     name: str
-    id: str
+    id: int
     cost: float
     quantity: Optional[float] = 1
     mark_code: Optional[str] = ""
